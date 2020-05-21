40f7ab86952daea16bdcf2b2da239bc01bc051ed
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-05-22 02:41:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 # imgur key
-client_id = 'YOUR_IMGUR_CLIENT_ID'
-client_secret = 'YOUR_IMGUR__CLIENT_SECRET'
-album_id = 'YOUR_IMGUR_ALBUM_ID'
-access_token = 'YOUR_IMGUR_ACCESS_TOKEN'
-refresh_token = 'YOUR_IMGUR_ACCESS_TOKEN'
+client_id = '18f064544f219ac'
+client_secret = 'c33fdd2c822d20be16ea2eb691242616f4759733'
+album_id = 'X0QL4'
+
 
 # line bot key
-line_channel_access_token = 'YOUR_CHANNEL_ACCESS_TOKEN'
-line_channel_secret = 'YOUR_CHANNEL_SECRET'
+line_channel_access_token = 'P7V+AiwPyztvIPr8EK+AlVWacCTa5FQWPNJs/0giVGt+0o985Srw35KwIYnIEPjqKgCVZomwbrcFt63vCUeUH9EPi2UwxqQ9XWraylX3/YHd/BPa/8W0wwZm36+XQ4LVuWAKhOopLbSrhHeprh9N7gdB04t89/1O/w1cDnyilFU='
+line_channel_secret = '59e352af8b15a1efddee622ce3c31d81'
